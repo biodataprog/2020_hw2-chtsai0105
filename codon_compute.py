16245580621737fe2f1e0eac05c9d0c79272e843
Cheng-Hung Tsai <ctsai085@ucr.edu>
2020-11-02 00:03:45 -0800

    add codes

--- codon_compute.py
+++ codon_compute.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, gzip, itertools
+import os, gzip, itertools, csv
 
 # this is code which will parse FASTA files
 # define what a header looks like in FASTA format
@@ -18,6 +18,47 @@ def aspairs(f):
             sequence = ''.join(line.strip() for line in group)
             yield seq_id, sequence
 
+def create_codon_dict():
+    codon_dict = dict()
+    base = ['A', 'T', 'G', 'C']
+    for first in base:
+        for second in base:
+            for third in base:
+                codon=first + second + third
+                codon_dict[codon] = 0
+    return codon_dict
+
+def genome_description(fasta_file):
+    codon_dict = create_codon_dict()
+    gene_count = 0
+    gene_length = 0
+    GC_count = 0
+    with gzip.open(fasta_file, "rt") as fh:
+        seqs = aspairs(fh)
+        for seq in seqs:
+            gene_count += 1
+            seqname  = seq[0]
+            seqstring= seq[1]
+            gene_length += len(seqstring)
+            for i in range(0, len(seqstring), 3):
+                codon = seqstring[i: i + 3]
+                codon_dict[codon] += 1
+                for nt in codon:
+                    if nt == 'G' or nt == 'C':
+                        GC_count += 1
+    codon_count = sum(codon_dict.values())
+    codon_freq = dict()
+    for k, v in codon_dict.items():
+        codon_freq[k] = v / codon_count
+
+    print('Genome: {}'.format(fasta_file))
+    print('Number of genes: {}'.format(gene_count))
+    print('Total length of the genes: {}'.format(gene_length))
+    print('GC content: {:2f}%'.format(100 * GC_count / gene_length))
+    print('Total number of codons : {}'.format(codon_count))
+    print()
+    return codon_freq
+
 url1="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/salmonella_enterica_subsp_enterica_serovar_typhimurium_str_lt2/cds/Salmonella_enterica_subsp_enterica_serovar_typhimurium_str_lt2.ASM694v2.cds.all.fa.gz"
 url2="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/mycobacterium_tuberculosis_h37rv/cds/Mycobacterium_tuberculosis_h37rv.ASM19595v2.cds.all.fa.gz"
 file1="Salmonella_enterica_subsp_enterica_serovar_typhimurium_str_lt2.ASM694v2.cds.all.fa.gz"
@@ -29,10 +70,11 @@ if not os.path.exists(file1):
 if not os.path.exists(file2):
     os.system("curl -O %s"%(url2))
 
-with gzip.open(file1,"rt") as fh:
-    seqs = aspairs(fh)
+codon_dict_sp1 = genome_description(file1)
+codon_dict_sp2 = genome_description(file2)
 
-    for seq in seqs:
-        seqname  = seq[0]
-        seqstring= seq[1]
-        print(seqname, " first 10 bases are ", seqstring[0:10])
+with open('codon_frequency.tsv', 'w') as fh:
+    writer = csv.writer(fh, delimiter="\t")
+    writer.writerow(('Codon', 'sp1_freq', 'sp2_freq'))
+    for key in codon_dict_sp1.keys():
+        writer.writerow((key, codon_dict_sp1[key], codon_dict_sp2[key]))
--- sevenless.py
+++ sevenless.py
@@ -4,3 +4,7 @@ start = 0
 end   = 99
 divisor=7
 print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+
+for i in range(start, end + 1):
+    if i%7 != 0 or i == 0:
+        print(i)
